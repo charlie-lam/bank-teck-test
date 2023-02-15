3bca9afd4ccfc0f9dfda8a60e977a3bacd5fe6b5
charlie lambert <char.lam@ntlworld.com>
2023-02-15 02:22:51 +0000

    added sorting by date in the display function

--- bank.js
+++ bank.js
@@ -12,33 +12,33 @@ class BankAccount {
         const year = parseInt(splitDate[2], 10) + 2000
         
         const dateObject = new Date(year, month, day)
-        return dateObject.toLocaleDateString("en-GB")
+        return dateObject
     }
 
     deposit(value, date){
         this.balance += value;
 
-        const formattedDate = this.dateFormatter(date)
+        const dateObject = this.dateFormatter(date)
         const formattedValue = value.toFixed(2)
         const formattedBalance = this.balance.toFixed(2)
 
-        this.transactions.unshift({credit: formattedValue , debit: '', date: formattedDate, balance: formattedBalance})
+        this.transactions.unshift({credit: formattedValue , debit: '', date: dateObject, balance: formattedBalance})
     }
 
     withdraw(value, date){
         this.balance -= value;
 
-        const formattedDate = this.dateFormatter(date)
+        const dateObject = this.dateFormatter(date)
         const formattedValue = value.toFixed(2).padStart(1).padEnd(1)
         const formattedBalance = this.balance.toFixed(2)
 
-        this.transactions.unshift({credit:'' , debit: formattedValue, date: formattedDate , balance:formattedBalance})
+        this.transactions.unshift({credit:'' , debit: formattedValue, date: dateObject , balance:formattedBalance})
     }
 
     display(){
         let result = 'date || credit || debit || balance';
-        this.transactions.forEach(transaction => {
-            result += `\n${transaction.date} || ${transaction.credit} || ${transaction.debit} || ${transaction.balance}`
+        this.transactions.sort((a,b) => b.date - a.date).forEach(transaction => {
+            result += `\n${transaction.date.toLocaleDateString("en-GB")} || ${transaction.credit} || ${transaction.debit} || ${transaction.balance}`
         });
         console.log(result)
         return result;
